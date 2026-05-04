e2dfc9072a852692df0c5b1eb58d62525342fbf7
EDC <cacildadias2@gmail.com>
2026-05-04 20:48:21 -0300

    Projeto Completo

--- dashboard.py
+++ dashboard.py
@@ -20,5 +20,48 @@ def main():
     if filtro_pais != "todos":
         data_filtrada = data_filtrada[data_filtrada['País']== filtro_pais]
 
-main()
+    gf_lucro_segmento = px.br(
+        data_filtrada.groupby('Segmento')['Lucro'].sum().reset_index(),
+        x='Segmento', y='Lucro',
+        title='Lucro Por Segmento',
+        color='Segmento',
+        text_auto=True
+    )
+    gf_lucro_segmento.update_layout(showlegend=False)
+
+    gf_vendas_tempo = px.line(
+        data_filtrada.groupby('Data')['Vendas Brutas'].sum().reset_index(),
+        x='Data', y='Vendas Brutas',
+        title='Vendas Brutas ao Longo do Tempo',
+        markers=True
+
+    )
      
+    gf_produtos_vendidos = px.pie(
+        data_filtrada.groupby('Produto')['Unidades Vendidas'].sum().reset_index(),
+        values='Unidades Vendidas', names='Produto',
+        title='Distribuição de Produtos Vendidos'
+     )
+    
+    custo_lucro_data = data_filtrada(['Segmento'])[['COGS', 'Lucro']].sum().reset_index().melt(
+        id_vars ='Segmento', value_vars = ['COGS', 'Lucro'])
+    custo_lucro_data['value_formatado'] = custo_lucro_data ['value'].apply(lambda x: f'R$ {x:.2}')
+
+    gf_custo =px.bar(
+       custo_lucro_data,
+       x='Segmento', y= 'value',
+       title='Relação Entre Custo e Lucro',
+       color='Relação Entre Custo e Lucro',
+       barmode='group',
+       text_auto=True
+    )
+
+    col1, col2 = st.columns(2)
+    col3, col4 = st.columns(2)
+    col1.plotly_chart(gf_lucro_segmento,use_container_width=True)
+    col2.plotly_chart(gf_vendas_vendas,use_container_width=True)
+    col3.plotly_chart(gf_produtos_vendidos,use_container_width=True)
+    col4.plotly_chart(gf_custo_lucro,use_container_width=True)
+
+main()    
+
